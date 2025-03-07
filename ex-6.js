f5ab972284b410aa4cec9fa27d5d94e346971ac8
Your Name <you@example.com>
2025-03-07 17:11:26 +0700

    feat: edit ex-1-7

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,10 @@
 let height = undefined;
 let result;
 
+result = height ?? "Height is not defined"
+
+// function xxx(height) {
+//     return height = undefined "Height is not defined"
+// }
+
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
